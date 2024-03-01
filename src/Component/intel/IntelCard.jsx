1dddb018dd7cc3176cb0e818f81e6a267a9c7cb2
unavailable16 <101920179+unavailable16@users.noreply.github.com>
2024-03-01 17:50:59 +0700

    Update IntelCard.jsx

--- src/Component/intel/IntelCard.jsx
+++ src/Component/intel/IntelCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GoTrash } from "react-icons/go";
 import "../List.css";
 
 const IntelCard = (props) => {
@@ -39,7 +40,7 @@ const IntelCard = (props) => {
             className="bordered secondary"
             onClick={() => handleDeleteClick(intel)}
           >
-            <span className="icon-alert inverse"></span>
+            <GoTrash />
             Delete
           </button>
         </div>
